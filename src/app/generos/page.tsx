cede862759143afad4223bbc4e99aa7a7af86b21
Lucas Barros <lucasweacked21@gmail.com>
2025-04-06 07:13:07 -0300

    feat: migracao da estrutura do projeto para Next.js App Router

--- src/app/generos/page.tsx
+++ src/app/generos/page.tsx
@@ -1,7 +1,8 @@
 import Header from "@/components/header/header";
 import Section from "@/components/section/section";
+import "@/app/globals.css";
 
-const RockPage = () => {
+const GenerosPage = () => {
   return (
     <>
       <Header />
@@ -40,4 +41,4 @@ const RockPage = () => {
   );
 };
 
-export default RockPage;
+export default GenerosPage;
